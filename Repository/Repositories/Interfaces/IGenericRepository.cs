8e566da6c5e8ce77a5199500932791d3980bcf46
nguyen <qnguyen.1014@gmail.com>
2025-11-15 04:26:08 +0700

    Squashed commit of the following:
    commit 039f8c59914955cedd1074b7b5828ff06789c45b
    Author: ThinhNguyenrt <171249327+ThinhNguyenrt@users.noreply.github.com>
    Date:   Sat Nov 15 11:02:30 2025 +0700
    
        refactor(login): refactor code for login

--- Repository/Repositories/Interfaces/IGenericRepository.cs
+++ Repository/Repositories/Interfaces/IGenericRepository.cs
@@ -8,91 +8,29 @@ using System.Threading.Tasks;
 
 namespace Repository.Repositories.Interfaces
 {
-    public interface IGenericRepository<T> : IDisposable where T : class
+    public interface IGenericRepository<T>  where T : class
     {
-        #region Get Async
+        Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate = null,
+ Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+ Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null);
 
-        Task<T> FirstOrDefaultAsync(
-            Expression<Func<T, bool>> predicate = null,
-            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-            Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null);
-
-        Task<TResult> FirstOrDefaultAsync<TResult>(
-            Expression<Func<T, TResult>> selector,
-            Expression<Func<T, bool>> predicate = null,
-            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-            Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null);
 
         Task<ICollection<T>> GetListAsync(
-            Expression<Func<T, bool>> predicate = null,
-            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-            Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null,
-            int? take = null);
-
-        Task<ICollection<TResult>> GetListAsync<TResult>(
-            Expression<Func<T, TResult>> selector,
-            Expression<Func<T, bool>> predicate = null,
-            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-            Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null,
-            int? take = null);
-
-        //Task<PagingResponse<T>> GetPagingListAsync(
-        //    Expression<Func<T, bool>> predicate = null,
-        //    Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-        //    Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null,
-        //    int page = 1,
-        //    int size = 10);
-
-        //Task<PagingResponse<TResult>> GetPagingListAsync<TResult>(
-        //    Expression<Func<T, TResult>> selector,
-        //    Expression<Func<T, bool>> predicate = null,
-        //    Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-        //    Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null,
-        //    int page = 1,
-        //    int size = 10);
-
-        Task<int> CountAsync(Expression<Func<T, bool>> predicate = null);
-
-        Task<T> SingleOrDefaultAsync(
-            Expression<Func<T, bool>> predicate = null,
-            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-            Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null);
-
-        Task<TResult> SingleOrDefaultAsync<TResult>(
-            Expression<Func<T, TResult>> selector,
-            Expression<Func<T, bool>> predicate = null,
-            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-            Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null);
-
-        #endregion
-
-        #region Insert
-
-        Task InsertAsync(T entity);
-
-        Task InsertRangeAsync(IEnumerable<T> entities);
-
-        #endregion
-
-        #region Update
-
-        void UpdateAsync(T entity);
-
-        void UpdateRange(IEnumerable<T> entities);
-
-        #endregion
-
-        #region Insert
-        void DeleteAsync(T entity);
-        void DeleteRangeAsync(IEnumerable<T> entities);
-        #endregion
-
-        #region queryable
-        IQueryable<T> CreateBaseQuery(
         Expression<Func<T, bool>> predicate = null,
         Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
         Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null,
+        int? take = null);
+
+
+        Task<int> CountAsync(Expression<Func<T, bool>> predicate = null);
+
+
+        Task InsertAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        IQueryable<T> CreateBaseQuery(Expression<Func<T, bool>> predicate = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null,
         bool asNoTracking = true);
-        #endregion
     }
 }
---
+++ Repository/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using BusinessObjects.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repositories.Interfaces
+{
+    public interface IUserRepository 
+    {
+        /// Get user by email with role information
+        Task<User> GetUserByEmailWithRoleAsync(string email);
+
+        /// Check if email already exists
+        Task<bool> EmailExistsAsync(string email);
+
+        /// Get user for authentication (includes role)
+        Task<User> GetUserForAuthenticationAsync(string email);
+    }
+}
