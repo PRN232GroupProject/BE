8377aaeefdfc8d87e0b3008e53fe9bec6c880ea8
nguyen <qnguyen.1014@gmail.com>
2025-11-15 18:26:37 +0700

    fix: API Definitions

--- ChemistryProjectPrep.API/Constants/ApiEndpointConstants.cs
+++ ChemistryProjectPrep.API/Constants/ApiEndpointConstants.cs
@@ -4,28 +4,26 @@
     {
         static ApiEndpointConstants() { }
 
-        public const string RootEndpoint = "/api";
-        public const string ApiVersion = "/v1";
-        public const string ApiEndpoint = RootEndpoint + ApiVersion;
+        public const string ApiEndpoint = "api";
         public const string AuthEndpoint = ApiEndpoint + "/auth";
         public const string UserEndpoint = ApiEndpoint + "/users";
 
         public static class Auth
         {
-            public const string LoginEndpoint = ApiEndpoint + "/login";
-            public const string RegisterEndpoint = ApiEndpoint + "/register";
-            public const string SendResetCodeEndpoint = ApiEndpoint + "/reset";
-            public const string VerifyResetCodeEndpoint = ApiEndpoint + "/verify/reset";
-            public const string DisableAccountEndpoint = ApiEndpoint + "/disable";
-            public const string VerifyDisableCodeEndpoint = ApiEndpoint + "/verify/disable";
-            public const string LogoutEndpoint = ApiEndpoint + "/logout";
+            public const string LoginEndpoint = AuthEndpoint + "/login";
+            public const string RegisterEndpoint = AuthEndpoint + "/register";
+            public const string SendResetCodeEndpoint = AuthEndpoint + "/reset";
+            public const string VerifyResetCodeEndpoint = AuthEndpoint + "/verify/reset";
+            public const string DisableAccountEndpoint = AuthEndpoint + "/disable";
+            public const string VerifyDisableCodeEndpoint = AuthEndpoint + "/verify/disable";
+            public const string LogoutEndpoint = AuthEndpoint + "/logout";
         }
 
         public static class User
         {
-            public const string GetUserEndpoint = UserEndpoint + "/{id}";
-            public const string GetCurrentUserEndpoint = ApiEndpoint + "/current";
             public const string GetAllUsersEndpoint = UserEndpoint;
+            public const string GetUserEndpoint = UserEndpoint + "/{id}";
+            public const string GetCurrentUserEndpoint = UserEndpoint + "/current";
             public const string GetUsersByRoleEndpoint = GetAllUsersEndpoint + "/{role}";
             public const string CreateUserEndpoint = UserEndpoint;
             public const string UpdateUserEndpoint = UserEndpoint;
