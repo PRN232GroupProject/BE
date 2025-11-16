718239a273bee6b307065c8e38d11788acc6b66d
KSuvery <luonglkse171576@fpt.edu.vn>
2025-11-16 23:38:47 +0700

    [ADD] feat: MediaController using Cloudinary

--- ChemistryProjectPrep.API/Program.cs
+++ ChemistryProjectPrep.API/Program.cs
@@ -1,14 +1,38 @@
 using ChemistryProjectPrep.API.Configurations;
+using DotNetEnv;
+using Microsoft.AspNetCore.Http.Features;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+Env.Load();
+
 // Configure all services
-builder.Services.AddApplicationServices();
+builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddCorsConfiguration();
 builder.Services.AddSwaggerConfiguration();
 builder.Services.AddJwtAuthenticationService(builder.Configuration);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+});
+
+// Configure form options for larger file uploads
+builder.Services.Configure<FormOptions>(options =>
+{
+    options.ValueLengthLimit = int.MaxValue;
+    options.MultipartBodyLengthLimit = 500_000_000; // 500MB
+    options.MultipartHeadersLengthLimit = int.MaxValue;
+});
 
 // Database Configuration
 DatabaseConfiguration.ConfigureDatabase(builder.Services, builder.Configuration);
